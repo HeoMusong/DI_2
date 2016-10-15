a2db91fafb981232af0ebb64300ed75e18b897fc
HeoMusong <anthdwkd@naver.com>
2016-10-15 15:09:35 +0900

    DI_(2)

--- src/main/java/kr/ac/hansung/spring/di/PetOwner.java
+++ src/main/java/kr/ac/hansung/spring/di/PetOwner.java
@@ -1,13 +1,17 @@
 package kr.ac.hansung.spring.di;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class PetOwner {
 	
+	@Autowired  // wiring by type
+	@Qualifier(value="qf_cat") // dog와 cat구분
+	@Resource(name ="dog") // wiring by name
 	private AnimalType animal;
 
-	public PetOwner(AnimalType animal) {
-		this.animal = animal;
-	}
-	
 	public void play(){
 		animal.sound();
 	}
